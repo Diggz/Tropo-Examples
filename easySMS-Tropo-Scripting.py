29e53442ba66e7b1647d6d53d5c73f9fee8e71f4
diggz <john.higgins@gmail.com>
2013-10-02 11:48:39 -0400

    Update easySMS-Tropo-Scripting.py
    added some additional comments

--- easySMS-Tropo-Scripting.py
+++ easySMS-Tropo-Scripting.py
@@ -1,9 +1,11 @@
 #----------
 # Easy SMS Send Example Written in Python and Runs on Tropo Scripting Platform
-# Initate this this using your Tropo API key assigned to this app like this:
+# 
+# Create a new Tropo Scripting App, copy/paste this code and assign a U.S. or Candian phone number
+# Send an SMS using the Tropo API key assigned to this app like this:
 # http://api.tropo.com/1.0/sessions?action=create&token=myAPIkey&myString=hello+world&myNumber=14075551212
-# (replace myAPIkey with the one assigned to your app)
-# (replace myNumber with the target number you want to receive the SMS)
+#  -- replace myAPIkey with the one assigned to your app --
+#  -- replace myNumber with the target number you want to receive the SMS --
 #----------
 
 message(myString, {
@@ -11,4 +13,4 @@ message(myString, {
   "network":"SMS", 
   "callerID":"+15162347664"})
   
-# ^^^ Make sure you set callerID to the phone number assigned to your Tropo App!!!
+# ^^^ Make sure you replace callerID to the phone number assigned to your Tropo App!!!
