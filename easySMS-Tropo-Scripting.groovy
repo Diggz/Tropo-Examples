a2d1a14adcc1f11263e9d9d7a768bf126e32b92c
diggz <diggz@tropo.com>
2013-10-02 16:11:22 -0400

    Initial Commit

---
+++ easySMS-Tropo-Scripting.groovy
@@ -0,0 +1,20 @@
+/* ----------
+ Easy SMS Send Example Written in Groovy and Runs on Tropo Scripting Platform
+ 
+ Create a new Tropo Scripting App, copy/paste this code and assign a U.S. or Candian phone number
+ Send an SMS using the Tropo API key assigned to this app like this:
+ http://api.tropo.com/1.0/sessions?action=create&token=myAPIkey&myString=hello+world&myNumber=14075551212
+ -- replace myAPIkey with the one assigned to your app --
+ -- replace myNumber with the target number you want to receive the SMS --
+ ----------
+*/
+
+message(myString, [
+  to: myNumber, 
+  network: "SMS", 
+  callerID: "+16075270142"])
+  
+ 
+// ^^^ Make sure you replace callerID to the phone number assigned to your Tropo App!!!
+
+?>
