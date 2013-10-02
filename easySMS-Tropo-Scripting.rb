0dd7e83101272c4ed409793ad3048b584c544790
diggz <john.higgins@gmail.com>
2013-10-02 14:56:54 -0400

    Update easySMS-Tropo-Scripting.rb

--- easySMS-Tropo-Scripting.rb
+++ easySMS-Tropo-Scripting.rb
@@ -1,9 +1,11 @@
 #----------
-# Easy SMS Send Example Written in Ruby and Runs on Tropo Scripting Platform
-# Initate this this using your Tropo API key assigned to this app like this:
+# Easy SMS Send Example Written in Python and Runs on Tropo Scripting Platform
+# 
+# Create a new Tropo Scripting App, copy/paste this code and assign a U.S. or Candian phone number
+# Send an SMS using the Tropo API key assigned to this app like this:
 # http://api.tropo.com/1.0/sessions?action=create&token=myAPIkey&myString=hello+world&myNumber=14075551212
-# (replace myAPIkey with the one assigned to your app)
-# (replace myNumber with the target number you want to receive the SMS)
+#  -- replace myAPIkey with the one assigned to your app --
+#  -- replace myNumber with the target number you want to receive the SMS --
 #----------
 
 message($myString, {
